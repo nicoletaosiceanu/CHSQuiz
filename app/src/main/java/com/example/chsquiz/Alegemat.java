9f9fa380fc3f11014147f70b511cfaf89191834d
osiceanu nicoleta <osiceanunicoleta@yahoo.com>
2020-12-20 03:52:36 +0200

    legecrane commit

---
+++ app/src/main/java/com/example/chsquiz/Alegemat.java
@@ -0,0 +1,18 @@
+
+package com.example.chsquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Alegemat extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alegematerie);
+
+
+    }
+}
--- app/src/main/java/com/example/chsquiz/ResultActivity.java
+++ app/src/main/java/com/example/chsquiz/ResultActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
     TextView t1,t2,t3;
+    Button b1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         t1=(TextView)findViewById(R.id.textView4);
         t2=(TextView)findViewById(R.id.textView5);
         t3=(TextView)findViewById(R.id.textView6);
+        b1 = (Button) findViewById(R.id.button1);
 
         Intent i=getIntent();
 
@@ -26,5 +30,12 @@ public class ResultActivity extends AppCompatActivity {
         t2.setText(correct);
         t3.setText(wrong);
 
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(com.example.chsquiz.ResultActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
